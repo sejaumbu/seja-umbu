94f2d95bd1caa1bfce99c236880b580fe21d0f23
Gustavo Melo <142261906+GustavoMeloFn@users.noreply.github.com>
2023-11-06 14:06:34 -0300

    add: footer & header components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import './assets/styles/Header.css';
+import './assets/styles/Footer.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Footer/>
+    </> 
+  )
+}
+
+export default App

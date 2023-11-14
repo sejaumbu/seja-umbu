c90c09e4d8ed336b06b5c91faa330db0c48dbcc2
ichcamile <camilesantana21@gmail.com>
2023-11-14 00:00:35 -0300

    adc demos no vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from  "./components/header/Header.jsx"
+import Header from "./components/header/Header.jsx"
 import Footer from "./components/footer/Footer.jsx"
 import BarraLateral from './components/barraLateral/BarraLateral.jsx';
 import Dashboard from './pages/dashboard/Dashboard.jsx';
-
+import UmchatF from './pages/umchat/UmchatF.jsx';
 
 function App() {
   return (
-    <div className='wrapper'>
-      <Dashboard/>
-    </div> 
+    <div className='teste'>
+      <UmchatF/>
+      <Header />
+      <Footer />
+
+
+    </div>
   )
 }
 

f80478cafad97d7cf40087fd0654787137305ad3
ichcamile <84331711+ichcamile@users.noreply.github.com>
2023-11-13 23:42:33 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from  "./components/header/Header.jsx"
 import Footer from "./components/footer/Footer.jsx"
-import UMChat from './components/UMchat/UMChat.jsx';
 
 function App() {
   return (
     <>
-      <UMChat/>
+      <Header/>
+      <Footer/>
     </> 
   )
 }

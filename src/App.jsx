5be5b332b4abd52c895fc5b0b792288d9a4be42f
Maicoln <maicolnp@gmail.com.github.com>
2023-11-15 10:44:07 -0300

    Adicionando a tela de Registro 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from  "./components/header/Header.jsx"
-import Footer from "./components/footer/Footer.jsx"
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import TelaRegistroT2 from "./components/telaRegistro2/TelaRegistroT2.jsx";
 
 function App() {
   return (
     <>
-      <Header/>
-      <Footer/>
-    </> 
-  )
+      <TelaRegistroT2 />
+    </>
+  );
 }
 
 export default App;

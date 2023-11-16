4eecf2d49bec08578d99ecb40e00b5eaf5905f6e
Gustavo-Melo <gustavo.melo.fn@gmail.com>
2023-11-16 02:08:47 -0300

    primeira-telaLogin

---
+++ src/components/telasLogin/Registro1.jsx
@@ -0,0 +1,24 @@
+import './Registro.css';
+export default function Registro1(){
+    return(
+        <div className="registro1">
+            <div className="logotipoRegistro">
+                <img src='https://s3-alpha-sig.figma.com/img/e266/100b/4d2bbf3cf55803c2ece524859a499814?Expires=1701043200&Signature=dNhceusc1dVCtnzIbFFXURzXqS2spL4lEF3X3J98~af54fpeK0LazraAFxvfNbc7rEzxUR9RJOkKr7Z3HFEsLFMV0ombkkwnQwcxEwCmlaksHXCRgYhp13Uj2CHFMUFDJRLZJrQJxP7cWxiNQfhSfa51s3v274io5kK2DWOlE1DU-YfA5kaXhNw6bCMLZgJFTyM1R8MssKTlcfHk8oDqHrLaZcrfZXoECEvxnU1OxsJ7gg1ihE0jJnW3M8a0KxaPqcrP22m9krB-ljZ3eWyiFSMMyMK9CHm8ErYRspMyPybKxQI7Lydb4uVQIi9kFXTaTrly-Or9inV7yoY7D8dfsg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'/>
+            </div>
+            <div className="containerRegistro1">
+                <div className="infoRegistro1">
+                    <span>Semeando</span>
+                    <span>Conexões,</span>
+                    <span>Colhendo</span>
+                    <span>Jardins</span>
+                </div>
+                <div className="contornoJardineiro">
+                    <div className="jardineiroRegistro1">
+                        <img className='pessoa1Registro1' src='https://s3-alpha-sig.figma.com/img/2cfc/d6a2/82ae62a6dab3bf9aed653092bf2a5fb3?Expires=1701043200&Signature=F~f1~9Rt0Gkmh68HQLDajELK3FvYrS-VBGakpQbSczD160EZfgXRJ8dPxD6drSTmwdaSXVeM7tJ0QU7koCfbR9-xSRgTNyTASTelURVZ4hunJIMb0HwJ9BrUjrAdz4thpmkXeri2WkFx8WbRZMJplqXwtLt8ID9ijwlY5RccygeV-6kPhVhsf7A24Q5dwkqY-zQ0Rueo5gKfu~qapsTO3YdC33O70A32kAzDTfeHc9VTTRcwLZ74CqJq6LRxmST5pwsLyRmuXzk0K1Wy1j0BXymbSmxGXq3dp1uJobVlshTtzeGaWT9BU45qANz4D~-cyTesLgiZO~Ovtl7yjRgyJA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'/>
+                        <img className='pessoa2Registro1' src='https://s3-alpha-sig.figma.com/img/5eb3/aa0e/b18cb8cbc97184f7183663128165783f?Expires=1701043200&Signature=j55oKwKjgpCCRU6spO-vMIryYjmLTlynQAsIgsdTX53XGzBoddANDUyaKVkD2tYQ0QKJda9IIZ-AGjqccNbG0YOYjJB8Sm0vdJtDZgKLHKXzMAuMqD-yeDQgMHCrz0lic4IeA4Gg0Koxbrry3emCxeiExJQ2tIgsy7EvWMYauQCCWI6PLmWVpLTkL3ZAYrH1XwPfRqaehM2JM740XpuSPOepJ~r0kJF30YvZCJRPj51suOpF6s2HkTtNlym5ayGwCcTiQCyOY6W9LLofusPxWdJcsKRakoQKXa6LJatV3P04DFNqmlPOE5UwMrjRnfrU70YUBmUi4QrK~hWlZAy9EQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}

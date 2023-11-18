35719cb44efdbe426b2601614474b0ad5068da32
ichcamile <camilesantana21@gmail.com>
2023-11-18 19:32:50 -0300

    add: componente incompleto

--- src/pages/registro1/TelaRegistro1.jsx
+++ src/pages/registro1/TelaRegistro1.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import LadoEsquerdoRegistro from "../../components/telasLogin/LadoEsquerdoRegistro"
 import ConteudoDireito1 from '../../components/ConteudoDireito1/ConteudoDireito1'
-
+import "../../pages/registro1/telaRegistro1.css"
 function TelaRegistro1() {
   return (
-    <div>
+    <div className='testeRegistro'>
         <LadoEsquerdoRegistro/>
         <ConteudoDireito1/>
     </div>

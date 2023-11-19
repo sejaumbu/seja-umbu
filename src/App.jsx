7bae94e70bfb17b232587c63c02249313a728750
Maicoln <maicolnp@gmail.com.github.com>
2023-11-19 15:42:53 -0300

    Adicionando: telaRegistro3

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import TelaRegistroT2 from "./components/telaRegistro2/TelaRegistroT2.jsx";
+import TelaRegistroT3 from "./components/telaRegistro3/telaRegistroT3.jsx";
 
 function App() {
   return (
     <>
-      <TelaRegistroT2 />
+      <TelaRegistroT3 />
     </>
   );
 }

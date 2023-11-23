7cfe169394d09cde9f74c4e241f4729323ad6e1c
Jalin Rabey <lucasbonfimdst@gmail.com>
2023-11-23 00:27:16 -0300

    aba de escalabilidade adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import BarraLateral from './components/barraLateral/BarraLateral.jsx';
 import Dashboard from './pages/dashboard/Dashboard.jsx';
 import UmchatF from './pages/umchat/UmchatF.jsx';
 import Portfolio from './pages/AbaPortfolio/AbaPortfolio.jsx';
-
+import UmBlog from './components/UMblog/UmBlog.jsx';
+import Escalabilidade from './pages/Escalabilidade/Escalabilidade.jsx';
 
 function App() {
   return (
     <div className='teste'>
-     <Portfolio/>
+     <Escalabilidade/>
     </div>
   )
 }

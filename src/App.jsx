f806cb21cf7c46bb353a0bd1bd4400999754d308
Maicoln <maicolnp@gmail.com.github.com>
2023-11-23 10:45:44 -0300

    Colocando a tipografia cantora one na identidade visual

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import LandingPage from './pages/landingPage/LandingPage';
 // import Notificacoes from './pages/Notificacoes/Notificacoes.jsx';
 // import Header from "./components/header/Header.jsx"
 // import Footer from "./components/footer/Footer.jsx"
 // import BarraLateral from './components/barraLateral/BarraLateral.jsx';
-// import Dashboard from './pages/dashboard/Dashboard.jsx';
+// import Dashboard from "./pages/dashboard/Dashboard.jsx";
 // import UmchatF from './pages/umchat/UmchatF.jsx';
 // import AbaDeAvaliacoes from './components/AbaDeAvaliações/AbaDeAvaliacoes.jsx';
 // import Perfil from './pages/perfil/Perfil.jsx';
-import IdentidadeVisualTela from "./pages/IdentidadeVisual/IdentidadeVisualTela.jsx"
+import IdentidadeVisualTela from "./pages/IdentidadeVisual/IdentidadeVisualTela.jsx";
+// import Escalabilidade from "./pages/Escalabilidade/Escalabilidade";
+// import UmBlog from "./components/UMblog/UmBlog";
+// import LandingPage from "./pages/landingPage/LandingPage";
 
 function App() {
   return (
-    <div className='teste'>
-      <IdentidadeVisualTela/>
+    <div className="teste">
+      <IdentidadeVisualTela />
       {/* <UmchatF/>
       <Header />
       <Footer      /> */}
-  </div>
-  )
+      {/* <Dashboard /> */}
+      {/* <UmBlog />
+      <Escalabilidade /> */}
+      {/*  <LandingPage /> */}
+    </div>
+  );
 }
 
 export default App;
-
-
--- src/pages/IdentidadeVisual/IdentidadeVisualTela.jsx
+++ src/pages/IdentidadeVisual/IdentidadeVisualTela.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import "./identidadeVisual.css";
 import ImagemFolhaEsquerda from "../../assets/imgFolhaEsquerda.svg";
 import ImagemLogoUmbu from "../../assets/imgLogoUmbu.svg";
@@ -7,84 +7,118 @@ import ImagemItensSomando from "../../assets/itensSomando.svg";
 import ImagemLogoUmbuPazinha from "../../assets/imgLogoUmbuPazinha.svg";
 import PaletaDeCores from "../../../src/assets/paleta-cores.svg";
 import AplicacaoVisual from "../../assets/aplicacaoElementoVisual.svg";
-import Header from "../../components/header/Header"
-import Footer from "../../components/footer/Footer"
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
 
 function IdentidadeVisualTela() {
-    return (
-        <div>
-            <Header></Header>
-            <div className="componenteIdentidade">
-                <div className="tituloIdentidade">
-                    <p className="titleV">Nossa Identidade Visual</p>
-                </div>
-                <div className="logoUmbu">
-                    <div className="imgLateral">
-                        <img src={ImagemFolhaEsquerda} alt="Imagem Folha Esquerda" />
-                    </div>
-                    <div className="imgLogo">
-                        <img src={ImagemLogoUmbu} alt="Imagem Logo Umbu" />
-                    </div>
-                    <div className="imgLateral">
-                        <img src={ImagemFolhaDireita} alt="Imagem Folha Direita" />
-                    </div>
-                </div>
-                <div className="desenvolvimentoLogo">
-                    <div>
-                        <p className="titulos">Desenvolvimento da Logo</p>
-                        <p className="subTitulo">
-                            Comunicando a natureza e a paixão pela jardinagem
-                        </p>
-                    </div>
-                    <div className="imgFormandoLogo">
-                        <img src={ImagemItensSomando} alt="Icones que formam a logo da umbu" />
-                        <img src={ImagemLogoUmbuPazinha} alt="Logo da umbu" />
-                    </div>
-                </div>
-                <div className="paletaDeCores">
-                    <div>
-                        <p className="titulos">Paleta de Cores</p>
-                        <p className="subTitulo">Matizes representativos da marca</p>
-                    </div>
-                    <div className="imgPaleta">
-                        <img className="paletaDeCoresFoto" src={PaletaDeCores} alt="" />
-                    </div>
-                </div>
-
-                <div className="fontes">
-                    <div>
-                        <p className="titulos">Fonte Utilizada</p>
-                        <p className="subTitulo">Formas de fonte para comunicar a mensagem</p>
-                        <div className="fontesMontserrat">
-                            <p className="fonteTitleM">MONTSERRAT</p>
-                            <p className="fonteSubtituloM">SUBTÍTULO</p>
-                            <p className="fonteParagrafoM">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                velit esse cillum dolore eu.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div className="aplicacaoElementoVisual">
-                    <div>
-                        <p className="titulos">APLICAÇÕES DE ELEMENTO VISUAL</p>
-                        <p className="subTitulo">Matizes representativos da marca</p>
-                    </div>
-                    <img src={AplicacaoVisual} alt="Aplicação elemento visual." />
-                </div>
-
-            </div>
-
-            <Footer />
-
-
+  return (
+    <div>
+      <Header></Header>
+      <div className="componenteIdentidade">
+        <div className="tituloIdentidade">
+          <p className="titleV">Nossa Identidade Visual</p>
         </div>
-    )
+        <div className="logoUmbu">
+          <div className="imgLateral">
+            <img src={ImagemFolhaEsquerda} alt="Imagem Folha Esquerda" />
+          </div>
+          <div className="imgLogo">
+            <img src={ImagemLogoUmbu} alt="Imagem Logo Umbu" />
+          </div>
+          <div className="imgLateral">
+            <img src={ImagemFolhaDireita} alt="Imagem Folha Direita" />
+          </div>
+        </div>
+        <div className="desenvolvimentoLogo">
+          <div>
+            <p className="titulos">Desenvolvimento da Logo</p>
+            <p className="subTitulo">
+              Comunicando a natureza e a paixão pela jardinagem
+            </p>
+          </div>
+          <div className="imgFormandoLogo">
+            <img
+              src={ImagemItensSomando}
+              alt="Icones que formam a logo da umbu"
+            />
+            <img src={ImagemLogoUmbuPazinha} alt="Logo da umbu" />
+          </div>
+        </div>
+
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className="paletaDeCores">
+          <div>
+            <p className="titulos">Paleta de Cores</p>
+            <p className="subTitulo">Matizes representativos da marca</p>
+          </div>
+          <div className="imgPaleta">
+            <img className="paletaDeCoresFoto" src={PaletaDeCores} alt="" />
+          </div>
+        </div>
+
+        <br />
+        <br />
+
+        <div className="fontes">
+          <div>
+            <p className="titulos">Fonte Utilizada</p>
+            <p className="subTitulo">
+              Formas de fonte para comunicar a mensagem
+            </p>
+
+            <br />
+            <br />
+            <br />
+
+            <div className="fontesCantoraOne">
+              <p className="fonteTitleC">CANTORA ONE</p>
+              <p className="fonteSubtituloC">SUBTÍTULO</p>
+              <p className="fonteParagrafoC">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu.
+              </p>
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <div className="fontesMontserrat">
+              <p className="fonteTitleM">MONTSERRAT</p>
+              <p className="fonteSubtituloM">SUBTÍTULO</p>
+              <p className="fonteParagrafoM">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <div className="aplicacaoElementoVisual">
+          <div>
+            <p className="titulos">APLICAÇÕES DE ELEMENTO VISUAL</p>
+            <p className="subTitulo">Matizes representativos da marca</p>
+          </div>
+          <img src={AplicacaoVisual} alt="Aplicação elemento visual." />
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
 
-export default IdentidadeVisualTela
+export default IdentidadeVisualTela;

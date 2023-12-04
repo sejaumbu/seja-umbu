ab3510d64aab5481b74330df9ab58bc6a83525f4
ichcamile <84331711+ichcamile@users.noreply.github.com>
2023-12-04 13:31:53 -0300

    ADD: ROTAS

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,48 @@
 
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import das Rotas
 import LandingPage from './pages/landingPage/LandingPage';
 import Notificacoes from './pages/Notificacoes/Notificacoes.jsx';
-// import Header from "./components/header/Header.jsx"
-// import Footer from "./components/footer/Footer.jsx"
-// import BarraLateral from './components/barraLateral/BarraLateral.jsx';
-// import Dashboard from './pages/dashboard/Dashboard.jsx';
-// import UmchatF from './pages/umchat/UmchatF.jsx';
+import Escalabilidade from "./pages/Escalabilidade/Escalabilidade.jsx"
+import Dashboard from './pages/dashboard/Dashboard.jsx';
+import UmchatF from './pages/umchat/UmchatF.jsx';
 // import AbaDeAvaliacoes from './components/AbaDeAvaliações/AbaDeAvaliacoes.jsx';
 // import Perfil from './pages/perfil/Perfil.jsx';
-
+import IdentidadeVisual from "./pages/IdentidadeVisual/IdentidadeVisualTela.jsx"
+import Umblog from "./pages/umblog/UmblogF.jsx"
 function App() {
   return (
-    <div className='teste'>
-      <LandingPage/>
-      {/* <UmchatF/>
-      <Header />
-      <Footer      /> */}
-  </div>
+
+    <div className="rot">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' Component={ LandingPage }/>
+          <Route path='/chat' Component={ UmchatF }/>
+          <Route path='/identidade' Component={ IdentidadeVisual }/>
+          <Route path='/dashboard' Component={ Dashboard }/>
+          <Route path='/escalabilidade' Component={ Escalabilidade }/>
+          <Route path='/notificacao' Component={ Notificacoes }/>
+          <Route path='/blog' Component={ Umblog }/>
+          <Route path='/login' Component={ Umblog }/>
+
+
+
+
+
+
+
+
+          
+        </Routes>
+    </BrowserRouter>
+
+
+    </div>
+    
   )
 }
 

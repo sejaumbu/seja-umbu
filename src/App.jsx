db2e0ae78bd579cb1fa1f5370d7d4dd48fe9cacb
ichcamile <84331711+ichcamile@users.noreply.github.com>
2023-12-04 22:52:20 -0300

    add: chat modificacoes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import LandingPage from './pages/landingPage/LandingPage';
 import Notificacoes from './pages/Notificacoes/Notificacoes.jsx';
 import Escalabilidade from "./pages/Escalabilidade/Escalabilidade.jsx"
 import Dashboard from './pages/dashboard/Dashboard.jsx';
-import UmchatF from './pages/umchat/UmchatF.jsx';
+import Umchat from './components/umchat/Umchat.jsx';
 // import AbaDeAvaliacoes from './components/AbaDeAvaliações/AbaDeAvaliacoes.jsx';
 // import Perfil from './pages/perfil/Perfil.jsx';
 import IdentidadeVisual from "./pages/IdentidadeVisual/IdentidadeVisualTela.jsx"
@@ -23,7 +23,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' Component={LandingPage} />
-          <Route path='/chat' Component={UmchatF} />
+          <Route path='/chat' Component={Umchat} />
           <Route path='/identidade' Component={IdentidadeVisual} />
           <Route path='/dashboard' Component={Dashboard} />
           <Route path='/escalabilidade' Component={Escalabilidade} />
--- src/components/UMchat/UMChat.jsx
+++ src/components/UMchat/UMChat.jsx
@@ -1,61 +1,50 @@
 import Mensagens from './ItemChat/Mensagens.jsx';
-import Contatos from './ItemChat/Contatos.jsx';  
+import Contatos from './ItemChat/Contatos.jsx';
 import { MdOutlineEmojiEmotions } from "react-icons/md";
 import { BsMic, BsSend } from "react-icons/bs";
 import './UMChat.css';
 
-export default function UMChat(){
-    return(
+export default function UMChat() {
+    return (
         <main className='umchat'>
             <div className='listaContatos'>
-                <h2>UMchat</h2> 
+                <h2 className='umChat'>UmChat</h2>
                 <Contatos
-                    iconesContatos='https://cdn-icons-png.flaticon.com/256/149/149071.png'
-                    nomeContato="Gustavo Melo"
-                    dataMensagem="12:00 | 21.11"
+                    iconesContatos="https://shotkit.com/wp-content/uploads/bb-plugin/cache/cool-profile-pic-matheus-ferrero-landscape-6cbeea07ce870fc53bedd94909941a4b-zybravgx2q47.jpeg"
+                    nomeContato="Rafaela Silva"
+                    dataMensagem="Há 30 minutos"
                 />
                 <Contatos
-                    iconesContatos='https://cdn-icons-png.flaticon.com/256/149/149071.png'
-                    nomeContato="Gustavo Melo"
-                    dataMensagem="12:00 | 21.11"
-                />  
-                <Contatos
-                    iconesContatos='https://cdn-icons-png.flaticon.com/256/149/149071.png'
-                    nomeContato="Gustavo Melo"
-                    dataMensagem="12:00 | 21.11"
-                />  
-                <Contatos
-                    iconesContatos='https://cdn-icons-png.flaticon.com/256/149/149071.png'
-                    nomeContato="Gustavo Melo"
-                    dataMensagem="12:00 | 21.11"
+                    iconesContatos="https://i.pinimg.com/originals/6b/b6/0f/6bb60f9f19d2ab89b7884be903f85576.jpg"
+                    nomeContato="Luciana Oliveira"
+                    dataMensagem="Há 1 hora"
                 />
                 <Contatos
-                    iconesContatos='https://cdn-icons-png.flaticon.com/256/149/149071.png'
-                    nomeContato="Gustavo Melo"
-                    dataMensagem="12:00 | 21.11"
+                    iconesContatos="https://i.pinimg.com/originals/c3/02/bd/c302bdbab47f484464c67978004413bc.jpg"
+                    nomeContato="Joana Santos"
+                    dataMensagem="Há 2 horas"
                 />
                 <Contatos
-                    iconesContatos='https://cdn-icons-png.flaticon.com/256/149/149071.png'
-                    nomeContato="Gustavo Melo"
-                    dataMensagem="12:00 | 21.11"
+                    iconesContatos="https://i.pinimg.com/originals/19/c7/4f/19c74f6a44c05aba8bf74c829fd0527d.jpg"
+                    nomeContato="Mariana Almeida"
+                    dataMensagem="Há 3 horas"
                 />
                 <Contatos
-                    iconesContatos='https://cdn-icons-png.flaticon.com/256/149/149071.png'
-                    nomeContato="Gustavo Melo"
-                    dataMensagem="12:00 | 21.11"
+                    iconesContatos="https://images.pexels.com/photos/13211891/pexels-photo-13211891.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    nomeContato="Rodrigo Pereira"
+                    dataMensagem="Há 4 horas"
                 />
-                <br/>
-                <p>Veja mais</p>    
+
             </div>
             <div>
-                <Mensagens/>
+                <Mensagens />
                 <div className='footerMessage'>
                     <div className="inputMessage">
-                        <MdOutlineEmojiEmotions/>
-                        <input placeholder='Escreva para o prestador de serviço...'/>
-                        <button className='sendButton'><BsSend/></button>
+                        <MdOutlineEmojiEmotions />
+                        <input placeholder='Escreva para o prestador de serviço...' />
+                        <button className='sendButton'><BsSend /></button>
                     </div>
-                    <button className='voiceButton'><BsMic/></button>
+                    <button className='voiceButton'><BsMic /></button>
                 </div>
             </div>
         </main>

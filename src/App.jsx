e3ac870fb3be55c10d894c9c189827302da99aac
ichcamile <84331711+ichcamile@users.noreply.github.com>
2023-12-04 22:12:29 -0300

    fix: files e componentes

--- src/App.jsx
+++ src/App.jsx
@@ -14,26 +14,13 @@ import UmchatF from './pages/umchat/UmchatF.jsx';
 // import Perfil from './pages/perfil/Perfil.jsx';
 import IdentidadeVisual from "./pages/IdentidadeVisual/IdentidadeVisualTela.jsx"
 import Umblog from "./pages/umblog/UmblogF.jsx"
+
+
 function App() {
   return (
 
-    <div className="rot">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' Component={ LandingPage }/>
-          <Route path='/chat' Component={ UmchatF }/>
-          <Route path='/identidade' Component={ IdentidadeVisual }/>
-          <Route path='/dashboard' Component={ Dashboard }/>
-          <Route path='/escalabilidade' Component={ Escalabilidade }/>
-          <Route path='/notificacao' Component={ Notificacoes }/>
-          <Route path='/blog' Component={ Umblog }/>
-          <Route path='/login' Component={ Umblog }/>
-
-
-          
-        </Routes>
-    </BrowserRouter>
-
+    <div className="rotas">
+      <Notificacoes/>
 
     </div>
     
--- src/pages/Notificacoes/Notificacoes.jsx
+++ src/pages/Notificacoes/Notificacoes.jsx
@@ -2,86 +2,57 @@ import React from 'react'
 import BarraLateral from '../../components/barraLateral/BarraLateral'
 import ItensFixo from '../../components/Notificações/BoxNotificacao/ItensFixo'
 import BoxNotificacao from '../../components/Notificações/BoxNotificacao/BoxNotificacao'
-import avatarM1 from "../../assets/avatarM1.svg"
-import avatarM7 from "../../assets/avatarM7.svg"
-import avatarH3 from "../../assets/avatarH3.svg"
-import avatarH6 from "../../assets/avatarH6.svg"
+// import avatarM1 from "../../assets/j"
+// import avatarM7 from "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+// import avatarH3 from "https://images.unsplash.com/photo-1480455624313-e29b44bbfde1?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+// import avatarH6 from "https://images.unsplash.com/photo-1501943416256-08140ba03763?q=80&w=1985&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 import { FaStar } from "react-icons/fa";
 
 
 function Notificacoes() {
   return (
     <div>
-      
-        <BarraLateral/>
-        <div className="conteudoNotificacao">
-      
-    <div className='notificacoes'>
-      <ItensFixo/>
-      <BoxNotificacao
-        imgPerfil={avatarM1}
-        nomeContratante="Rafaella Costa"
-        nota1={<FaStar />}
-        nota2={<FaStar />}
-        nota3={<FaStar />}
-        nota4={<FaStar />}
-        nota5={<FaStar />}
-        comentario="O trabalho do jardineiro em nossa casa foi verdadeiramente excepcional! Cada flor e arbusto parecem ter ganhado vida sob seus cuidados habilidosos. O jardim agora é um oásis de cores vibrantes e harmonia natural, um testemunho do seu talento e dedicação. Estamos encantados com a transformação que você trouxe ao nosso espaço! "
-        data="Há 15 minutos"
-      />
 
-      <BoxNotificacao
-        imgPerfil={avatarH3}
-        nomeContratante="Rodigo da Silva"
-        nota1={<FaStar />}
-        nota2={<FaStar />}
-        nota3={<FaStar />}
-        nota4={<FaStar />}
-        
-        comentario="O trabalho dele foi muito bom, realmente mudou o aspecto do meu jardim para melhor, porem ele se atrasou muito, e acabei perdendo um compromisso"
-        data="Há 7 horas"
-      />
+      <BarraLateral />
+      <div className="conteudoNotificacao">
 
-      
-      <BoxNotificacao
-        imgPerfil={avatarH6}
-        nomeContratante="Mario Augusto"
-        nota1={<FaStar />}
-        nota2={<FaStar />}
-        nota3={<FaStar />}
-        nota4={<FaStar />}
-        nota5={<FaStar />}
-        comentario="O jardineiro arrasou aqui em casa! O jardim tá parecendo um sonho, sério! As plantas tão radiantes, as cores tão vivas, nem parece o mesmo lugar. Esse cara é mágico, transformou nosso cantinho num paraíso verde! Tamo amando demais o trampo dele!"
-        data="Há 4 dias"
-      />
+        <div className='notificacoes'>
+          <ItensFixo />
+          <BoxNotificacao
+            imgPerfil="https://shotkit.com/wp-content/uploads/bb-plugin/cache/cool-profile-pic-matheus-ferrero-landscape-6cbeea07ce870fc53bedd94909941a4b-zybravgx2q47.jpeg"
+            nomeContratante="Rafaela  Silva"
+            comentario="Oi, estou pensando em criar um jardim vertical na minha varanda. Você tem alguma sugestão de plantas que funcionariam bem nesse espaço?"
+            data="Há 30 minutos"
+          />
+          <BoxNotificacao
+            imgPerfil="https://i.pinimg.com/originals/6b/b6/0f/6bb60f9f19d2ab89b7884be903f85576.jpg"
+            nomeContratante="Luciana Oliveira"
+            comentario="Olá! Preciso de ajuda para identificar uma planta no meu jardim. Você poderia me orientar sobre como cuidar dela?"
+            data="Há 1 hora"
+          />
+          <BoxNotificacao
+            imgPerfil="https://i.pinimg.com/originals/c3/02/bd/c302bdbab47f484464c67978004413bc.jpg"
+            nomeContratante="Joana Santos"
+            comentario="Estou planejando renovar meu jardim. Alguma sugestão de design ou plantas que estão em alta?"
+            data="Há 2 horas"
+          />
+          <BoxNotificacao
+            imgPerfil="https://i.pinimg.com/originals/19/c7/4f/19c74f6a44c05aba8bf74c829fd0527d.jpg"
+            nomeContratante="Mariana Almeida"
+            comentario="Oi, gostaria de agendar uma visita para avaliação do meu jardim. Quando você estaria disponível?"
+            data="Há 3 horas"
+          />
+          <BoxNotificacao
+            imgPerfil="https://images.pexels.com/photos/13211891/pexels-photo-13211891.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            nomeContratante="Rodrigo Pereira"
+            comentario="Oi, estou impressionado com o último trabalho que você fez no meu jardim. Muito obrigado pelo excelente serviço!"
+            data="Há 4 horas"
+          />
 
-      
-      <BoxNotificacao
-        imgPerfil={avatarM7}
-        nomeContratante="Maria Eduarda"
-        nota1={<FaStar />}
-        nota2={<FaStar />}
-        nota3={<FaStar />}
-        nota4={<FaStar />}
-        nota5={<FaStar />}
-        comentario="
-        Esse jardinheiro é simplesmente fenomenal! A árvore que ele plantou se integrou perfeitamente ao jardim. As podas nas flores foram precisas, o lugar ganhou nova vida. É incrível ver como ele transformou nosso espaço, é um verdadeiro mago das plantas! Estamos maravilhados com o resultado!"
-        data="Há 2 semanas"
-      />
 
-      
-      <BoxNotificacao
-        imgPerfil={avatarM1}
-        nomeContratante="Beatriz Castro"
-        nota1={<FaStar />}
-        nota2={<FaStar />}
-        nota3={<FaStar />}
-        comentario="Serviço decente, mas faltou atenção aos detalhes. Algumas plantas não foram podadas corretamente. A comunicação foi eficaz, mas a execução do trabalho precisa de mais cuidado."
-        data="Há 1 mês"
-      />
+        </div>
+      </div>
     </div>
-    </div>
-  </div>
   )
 }
 

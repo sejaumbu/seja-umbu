0724dc91f062132d67f70d42a70fd98467d6b044
Ingrid Vitória Santos <142261730+IngridvS@users.noreply.github.com>
2023-12-06 21:08:54 -0300

    adc: implementacao-alteracao-identidade

--- src/pages/IdentidadeVisual/IdentidadeVisualTela.jsx
+++ src/pages/IdentidadeVisual/IdentidadeVisualTela.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./identidade.css";
-import folhaDireita from"../../assets/usadosIdentidade/folhaDireita.png";
+import folhaDireita from "../../assets/usadosIdentidade/folhaDireita.png";
 import folhaEsquerda from "../../assets/usadosIdentidade/folhaEsquerda.png";
 import ItensSomados from "../../assets/usadosIdentidade/ItensSomados.png";
 import logoCentral from "../../assets/usadosIdentidade/logoCentral.png";
@@ -18,7 +18,7 @@ function IdentidadeVisualTela() {
                     <p className="titleV">Nossa Identidade Visual</p>
                 </div>
 
-                <div className='fundo'>  
+                <div className='fundo'>
                     <div className="logoUmbu">
                         <div className="imgLateral">
                             <img src={folhaEsquerda} alt="Imagem Folha Esquerda" />
@@ -34,48 +34,62 @@ function IdentidadeVisualTela() {
 
 
                 <div className="desenvolvimentoLogo">
-                    
+
                     <div className="text1">DESENVOLVIMENTO DA LOGO </div>
                     <div className="Textsub">Comunicando a natureza e a paixão pela jardinagem </div>
-                    
+
                     <div className="imgFormandoLogo">
                         <img className="logoI" src={ItensSomados} alt="Icones que formam a logo da umbu" />
-                        <img className="logoU"src={logoUmbu} alt="Logo da umbu" />
+                        <img className="logoU" src={logoUmbu} alt="Logo da umbu" />
                     </div>
-
+                    {/* <<<<<<=========>>>>>>> */}
                 </div>
 
-                
+
+
                 <div className="paletaDeCores">
                     <div>
                         <div className="TituloP">Paleta de Cores </div>
                         <div className="sub">Matizes representativos da marca </div>
                     </div>
 
-
-                    <div className="imgPaleta">
-                        
-                    </div>
+                    <div className="imgPaleta"></div>
 
 
                 </div>
 
                 <div className="fontes">
-                    <div>
-                        <p className="titulos">Fonte Utilizada</p>
-                        <p className="subTitulo">Formas de fonte para comunicar a mensagem</p>
-                        <div className="fontesMontserrat">
-                            <p className="fonteTitleM">MONTSERRAT</p>
-                            <p className="fonteSubtituloM">SUBTÍTULO</p>
-                            <p className="fonteParagrafoM">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                velit esse cillum dolore eu.
-                            </p>
+
+                    <div className="titutoF">Fonte Utilizada </div>
+                    <div className="subF">Formas de fonte para comunicar a mensagem </div>
+
+                    <div className="fontesMontserrat">
+
+                        <div className='FontEs'>
+                            <div className="titulo1">CANTORA ONE BOLD</div>
+                            <p className="sub1">ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 1234567890 ! @#$%^&amp;*()</p>
+                        
+                        
+                            <div className="titulo2">CANTORA ONE</div>
+                            <p className="sub2">ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 1234567890 ! @#$%^&amp;*()</p>
+                        
+
+                            <div className="titulo3">GUIA DE FONTE</div>
+                            <div className="sub3">Combinação e dimensionamento adequados</div> 
                         </div>
+
+                        <div className='parte2'>
+                            <div className="titulo4">Montserrat</div>
+                            <div className="sub4">SUBTÍTULO</div> 
+                            <div className="paragrafo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                            velit esse cillum dolore eu.</div> 
+                        </div>
+                        
                     </div>
+
                 </div>
 
 
@@ -84,7 +98,9 @@ function IdentidadeVisualTela() {
                         <p className="titulos">APLICAÇÕES DE ELEMENTO VISUAL</p>
                         <p className="subTitulo">Matizes representativos da marca</p>
                     </div>
-                    <img src={Aplicacao} alt="Aplicação elemento visual." />
+                    <div className='espaçamentoCima'>
+                    <img className="finalPagina"src={Aplicacao} alt="Aplicação elemento visual." />
+                    </div>
                 </div>
 
             </div>

082651c7804d44a5fe6dcadea6813a1268fabb45
Gustavo Melo <142261906+GustavoMeloFn@users.noreply.github.com>
2023-12-14 14:22:07 -0300

    fix: espaçamento dos elementos

--- src/components/telasRegistroContratante/telaRegistroContratante1/TelaRegistroContratanteT2.jsx
+++ src/components/telasRegistroContratante/telaRegistroContratante1/TelaRegistroContratanteT2.jsx
@@ -5,6 +5,7 @@ import { FaFacebook } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
 
 function TelaRegistroContratanteT2(props) {
+
   return (
     <div className="containerT2">
         <form className="formT2">
